c30e11694ceace7a2fb158ca1003a2333169a11a
Somansu <psomansu@gmail.com>
2025-04-30 22:25:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -112,3 +112,82 @@ function displayData(userData){
 }
 
 getUserData(url+'somPNG');
+
+
+const dlBtn = document.querySelector('.dlBtn');
+const dlIcon = document.querySelector('[dlIcon]');
+const dlPara = document.querySelector('[dlPara]');
+const wrapper = document.querySelector('.wrapper');
+const main = document.querySelector('.main');
+const searchBar = document.querySelector('.search-Bar');
+const statBar = document.querySelector('.profile-info');
+
+// Creating Dark mode and light mode applying functions
+
+function applyLight(){
+    dlIcon.classList.remove('fa-sun');
+    dlIcon.classList.add('fa-moon');
+    dlPara.innerText= "Dark";
+
+    wrapper.style.backgroundColor = '#F0F2F5';
+    wrapper.style.color = 'Black';
+    username.style.color = 'Black';
+
+    main.style.backgroundColor = "#F9F9F9";
+    main.style.boxShadow = "0 10px 20px rgba(0, 0, 0, 0.3)";
+    searchBar.style.boxShadow = "0 10px 20px rgba(0, 0, 0, 0.3)";
+    searchBar.style.backgroundColor = "#F9F9F9";
+    statBar.style.backgroundColor = "#CCCCCC";
+    searchBtn.style.backgroundColor = '#E5E7EB';
+}
+
+function applyDark(){
+    dlIcon.classList.remove('fa-moon');
+    dlIcon.classList.add('fa-sun');
+    dlPara.innerText= "Light";
+
+
+    wrapper.style.backgroundColor = '#0D1117';
+    wrapper.style.color = 'White';
+    username.style.color = 'White';
+    main.style.backgroundColor = "#161B22";
+    main.style.boxShadow = "0 12px 20px rgba(0, 0, 0, 0.6)";
+    searchBar.style.boxShadow = "0 12px 20px rgba(0, 0, 0, 0.6)";
+    searchBar.style.backgroundColor = "#161B22";
+    statBar.style.backgroundColor = "#0B0F14";
+}
+
+dlBtn.addEventListener('click',()=>{
+    if(dlPara.innerText === "Dark"){
+        applyDark();
+    }
+    else if(dlPara.innerText === "Light"){
+        applyLight();
+    }
+})
+
+
+// checking preffered mode of a user's pc
+const preferMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+if(preferMode){
+    console.log('dark');
+    applyDark();
+}
+else{
+    applyLight();
+    console.log("light");
+}
+
+
+// For change in dark or light mode
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change',(e)=>{
+    if(e.matches){
+        applyDark();
+        console.log("Prefers dark scheme");
+    }
+    else{
+        applyLight();
+        console.log("prefers light theme");
+    }
+})
+
